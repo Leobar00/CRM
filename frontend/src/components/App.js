0b0c1adefbc72409c5e0cea82b46a65df8ace761
Leonardo Barbieri <leonardo.barbieri@boraso.com>
2024-12-28 19:47:09 +0100

    Base front and back

---
+++ frontend/src/components/App.js
@@ -0,0 +1,14 @@
+import logo from './../logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Test header
+      </header>
+    </div>
+  );
+}
+
+export default App;
